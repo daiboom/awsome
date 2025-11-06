fe93142ecde23f2842f8dd2bbb3852051e8eec76
choedaebeom <chleoqja0125@nate.com>
2025-11-06 21:36:14 +0900

    init

---
+++ src/main/java/org/startlight/awsome/AwsomeApplication.java
@@ -0,0 +1,13 @@
+package org.startlight.awsome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/startlight/awsome/AwsomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.startlight.awsome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
