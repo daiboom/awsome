8dc4aa56a03942d0b3858ed726d81f753d8fa1ee
choedaebeom <chleoqja0125@nate.com>
2025-11-07 14:48:52 +0900

    밸리데이터 추가

--- src/main/java/org/startlight/awsome/bean/User.java
+++ src/main/java/org/startlight/awsome/bean/User.java
@@ -2,6 +2,8 @@ package org.startlight.awsome.bean;
 
 import java.util.Date;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +11,10 @@ import lombok.Data;
 @AllArgsConstructor
 public class User {
   private Integer id;
+
+  @Size(min = 2, message = "Name must be at least 2 characters long")
   private String name;
+
+  @Past(message = "Join date must be in the past")
   private Date joinDate;
 }
