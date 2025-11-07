1fb0d03120d2afed3767053fbe610c42856846e8
choedaebeom <chleoqja0125@nate.com>
2025-11-07 14:49:02 +0900

    i18n 세팅

--- src/main/java/org/startlight/awsome/AwsomeApplication.java
+++ src/main/java/org/startlight/awsome/AwsomeApplication.java
@@ -1,12 +1,24 @@
 package org.startlight.awsome;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class AwsomeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AwsomeApplication.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
     }
 }
