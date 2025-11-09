a1e82cca340933f3dd47ccd713f8bc6782242dc8
choedaebeom <chleoqja0125@nate.com>
2025-11-09 17:47:58 +0900

    서버 시작시 데이터 초기화하기

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users (name, join_date, password, ssn) values ('User1', '2024-01-01', 'pass1', '901010-1111111');
+insert into users (name, join_date, password, ssn) values ('User2', '2024-01-02', 'pass2', '901010-1111112');
+insert into users (name, join_date, password, ssn) values ('User3', '2024-01-03', 'pass3', '901010-1111113');
---
+++ src/main/resources/test.sql
@@ -0,0 +1,3 @@
+insert into users (name, join_date, password, ssn) values ('TestUser1', '2024-01-01', 'pass1', '901010-1111111');
+insert into users (name, join_date, password, ssn) values ('TestUser2', '2024-01-02', 'pass2', '901010-1111112');
+insert into users (name, join_date, password, ssn) values ('TestUser3', '2024-01-03', 'pass3', '901010-1111113');
