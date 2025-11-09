52d6cb04eb070376651600be1f74420b2d594c5d
choedaebeom <chleoqja0125@nate.com>
2025-11-09 18:00:14 +0900

    인증 제외 패스 설정

--- src/main/java/org/startlight/awsome/config/SecurityConfig.java
+++ src/main/java/org/startlight/awsome/config/SecurityConfig.java
@@ -1,30 +1,36 @@
-// package org.startlight.awsome.config;
+package org.startlight.awsome.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-// @Configuration
-// public class SecurityConfig {
-// @Bean
-// UserDetailsService userDetailsService() {
+@Configuration
+public class SecurityConfig {
+    @Bean
+    UserDetailsService userDetailsService() {
 
-// InMemoryUserDetailsManager userDetailsManager = new
-// InMemoryUserDetailsManager();
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
 
-// UserDetails user = User.withUsername("user")
-// .password(passwordEncoder().encode("passw0rd"))
-// .authorities("read").build();
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("passw0rd"))
+                .authorities("read").build();
 
-// userDetailsManager.createUser(user);
-// return userDetailsManager;
-// }
+        userDetailsManager.createUser(user);
+        return userDetailsManager;
+    }
 
-// @Bean
-// BCryptPasswordEncoder passwordEncoder() {
-// return new BCryptPasswordEncoder();
-// }
-// }
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/h2-console/**");
+    }
+}
